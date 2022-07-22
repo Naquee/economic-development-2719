4b3584938879735157d53fe55b3f4ef712e0ee52
unknown <naqueealam99@gmail.com>
2022-07-23 04:41:58 +0530

    day4

--- economic-devlopment/src/components/pages/Login.jsx
+++ economic-devlopment/src/components/pages/Login.jsx
@@ -1,36 +1,91 @@
-import React from 'react'
-import { Input,Stack ,Button} from '@chakra-ui/react'
 
-const Login = () => {
-  return (
-    <div style={{width:'50%',
-    margin:'auto',
-    marginTop:"6rem"
-    }}>
-    <p style={{fontSize:"13px"}}>LOGIN OR CREATE ACCOUNT</p>
-    
-    <span style={{fontSize:"13px"}}>Email address</span>
-    <br />
-    <Stack style={{width:"50%",margin:"auto",border:'black',height:"2rem"}} spacing={3}>
-    
-  <Input size='md' />
-</Stack>
-<br />
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
-<Button
-  size='md'
-  height='2rem'
-  width='50%'
-  border='1px'
-//   borderColor='blsck.'
-  color="white"
-  background='black'
->
-  Continue
-</Button>
+import "./login.css";
 
+function Login() {
+  // React States
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  // User Login info
+  const database = [
+    {
+      username: "naki@gmail.com",
+      password: "123456"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  // Sign In
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      if (userData.password !== pass.value) {
+        // Invalid password
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      // Username not found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+  };
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label>email address </label>
+          <input type="text" name="uname" required />
+          {renderErrorMessage("uname")}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {renderErrorMessage("pass")}
+        </div>
+        <div className="button-container">
+          <input type="submit"/>
+        </div>
+      </form>
     </div>
-  )
+  );
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title"><p>LOGIN OR CREATE ACCOUNT</p></div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- economic-devlopment/src/components/pages/Menspage.jsx
+++ economic-devlopment/src/components/pages/Menspage.jsx
@@ -1,8 +1,121 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
+import Footer from '../Footer/Footer';
+import './Menspage.css';
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
 
 const Menspage = () => {
+  const [mens,setMens]=useState([])
+  useEffect(()=>{
+    const fetchPage = async () =>{
+      let res=await fetch("http://localhost:8080/menspage")
+      let data=await res.json();
+      console.log(data)
+      setMens(data)
+
+    }
+    fetchPage();
+
+  },[])
   return (
-    <div>Menspage</div>
+    <>
+    <div className='topcontainer'>
+    <div className='leftbar'>
+
+    <div style={{marginLeft:"-2rem"}}><Checkbox  backgrounColor='black' defaultChecked>Check</Checkbox></div>
+    <div style={{float:'left',marginTop:"1rem"}}>
+    <h2 style={{color:'red'}}>Topwear</h2>
+    <p>T-shirt</p>
+    <p>Casual-Shirt</p>
+    <p>Sweater</p>
+    <p>Jacket</p>
+    <p>Blazer</p>
+    <p>Suit</p>
+    <p>Rain jacket</p>
+    </div>
+
+    <div style={{float:'left',marginTop:"1rem"}}>
+    <h2 style={{color:'red'}}>Bottomwear</h2>
+    <p>Jeans</p>
+    <p>Casual Trouar</p>
+    <p>Formal Trouar</p>
+    <p>Shorts</p>
+    <p>Track Pants </p>
+    
+    </div>
+    <div style={{float:'left',marginTop:"1rem"}}>
+    <h2 style={{color:'red'}}>Footwear</h2>
+    <p>Casual Shoes</p>
+    <p>Sport shoes</p>
+    <p>Formal shoes</p>
+    <p>Sneaker</p>
+    
+    
+    </div>
+
+
+
+    </div>
+
+
+  
+    <div className='middlepage'>
+    {
+      mens.map((page)=>(
+        <div className='container'>
+        <div>
+          <img src={page.image} alt="" />
+        </div>
+       <div>
+       <p>{page.title}</p>
+        <p>{page.para}</p>
+        <p>{page.prod}</p>
+        <p style={{backgrounColor:"teal"}}>{page.price}</p>
+        <button style={{backgrounColor:'red'}}>Add to cart</button>
+       </div>
+        </div>
+      ))
+    }
+
+    </div>
+    
+    <div className='rightbar'>
+    <h4>Sort</h4>
+    <p>Latest Arrival</p>
+    <p>Tranding</p>
+    <p>Price Low To High</p>
+    <p>Price high To Low</p>
+    <br />
+    <h4 style={{fontWeight:"bold"}}>Color</h4>
+   <select name="All color" id="">
+   <option value="">All Color</option>
+    <option value="">black</option>
+    <option value="">Brown</option>
+    <option value="">Blue</option>
+    <option value="">Brown</option>
+    <option value="">Burgandy</option>
+    <option value="">Grey</option>
+    <option value="">Green </option>
+    <option value="">Navy</option>
+    <option value="">Orange</option>
+    <option value="">Pink</option>
+    <option value="">Purpal</option>
+    <option value="">Tan</option>
+    <option value="">White</option>
+    <option value="">Yellow</option>
+
+
+   </select>
+      
+
+     
+    </div>
+    </div>
+    <Footer/>
+
+
+    
+    </>
   )
 }
 
